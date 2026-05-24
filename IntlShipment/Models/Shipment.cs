0340e981428ee9199675a486590fb6b97c730a34
Migue <silvamedinamiguel@gmail.com>
2026-05-24 00:06:42 -0500

    feat: add Shipment and User models

---
+++ IntlShipment/Models/Shipment.cs
@@ -0,0 +1,21 @@
+﻿namespace IntlShipment.Models
+{
+    public class Shipment
+    {
+        public int Id { get; set; }
+        public string? NumeroGuia { get; set; }
+        public string? PaisOrigen { get; set; }
+        public string? PaisDestino { get; set; }
+        public string? CiudadOrigen { get; set; }
+        public string? CiudadDestino { get; set; }
+        public string? NombreRemitente { get; set; }
+        public string? NombreDestinatario { get; set; }
+        public string? DescripcionMercancia { get; set; }
+        public decimal PesoKg { get; set; }
+        public string? Estado {  get; set; }
+        public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+        public DateTime FechaEstimadaEntrega { get; set; } = DateTime.UtcNow;
+
+
+    }
+}
