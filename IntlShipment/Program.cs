350afbab79e1c808c9ed35bd52f69a5d0202c76f
Migue <silvamedinamiguel@gmail.com>
2026-05-24 00:08:14 -0500

    feat: configure SQL Server connection in Program.cs

--- IntlShipment/Program.cs
+++ IntlShipment/Program.cs
@@ -1,4 +1,11 @@
+using IntlShipment.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//SQL SERVER
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
